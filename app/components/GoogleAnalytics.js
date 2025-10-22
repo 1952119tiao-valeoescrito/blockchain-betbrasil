9fa31dc012b4a25436ac778f4eb3bd55d49d0c72
Sebastiao <1952119-tio-valeoescrito-sfchagasfilho@yahoo.com.br.noreply.github.com>
2025-10-22 19:04:26 -0300

    feat: add Google Analytics tracking

---
+++ app/components/GoogleAnalytics.js
@@ -0,0 +1,40 @@
+// components/GoogleAnalytics.js
+
+'use client';
+
+import Script from 'next/script';
+
+const GoogleAnalytics = () => {
+  const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+  
+  if (!gaMeasurementId) {
+    console.warn('⚠️ GA Measurement ID não encontrado');
+    return null;
+  }
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaMeasurementId}', {
+              page_title: document.title,
+              page_location: window.location.href
+            });
+          `,
+        }}
+      />
+    </>
+  );
+};
+
+export default GoogleAnalytics;
